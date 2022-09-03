ecad4899ae71047c036a4c5ecc6d1a82fed96b43
A-Oka-jr <iscoa020@gmail.com>
2022-09-03 11:53:31 +0200

    split multer middleware in separate file

---
+++ middleware/upload_single_file.js
@@ -0,0 +1,29 @@
+const multer = require("multer");
+
+const storage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    cb(null, "uploads/");
+  },
+  filename: (req, file, cb) => {
+    const uniqueSuffix = Date.now() + "-" + file.originalname;
+    cb(null, file.fieldname + "-" + uniqueSuffix);
+  },
+});
+
+const imageFileFilter  = (req, file, cb) => {
+  if (
+    file.mimetype === "image/jpeg" ||
+    file.mimetype === "image/jpg" ||
+    file.mimetype === "image/png"
+  ) {
+    cb(null, true);
+  } else {
+    cb(null, false);
+  }
+};
+
+exports.upload = multer({
+  storage: storage,
+  fileFilter: imageFileFilter,
+  limits: { fileSize: 1024 * 1024 * 5 },
+});
--- routes/users.js
+++ routes/users.js
@@ -2,34 +2,11 @@ const express = require("express");
 const db = require("../database/db");
 const userController = require("../controllers/user_controller");
 const router = express.Router();
-const multer = require('multer');
+const { upload } = require("../middleware/upload_single_file");
 
-const storage = multer.diskStorage({
-    destination: (req, file, cb) => {
-        cb(null, 'uploads/');
-    },
-    filename: (req, file, cb) => {
-        const uniqueSuffix = Date.now() + '-'  + file.originalname
-        cb(null, file.fieldname + '-' + uniqueSuffix)
-    }
-})
 
-const fileFilter = (req, file, cb) => {
-    if (file.mimetype === 'image/jpeg' || file.mimetype === 'image/jpg' || file.mimetype === 'image/png') {
-        cb(null, true)
-    } else {
-        cb(null, false)
-    }
-}
-
-const upload = multer({
-    storage: storage,
-    limits: {fileSize: 1024 * 1024 * 5},
-    fileFilter:fileFilter
-})
-// Get all users
 router.get("/", userController.getAllUsers);
-router.post("/", upload.single('personal_image'), userController.newUser);
+router.post("/", upload.single("personal_image"), userController.newUser);
 router.get("/:userId", userController.getOneUser);
 router.put("/:userId",upload.single('personal_image'), userController.updateUser);
 router.delete("/:userId", userController.deleteUser);
